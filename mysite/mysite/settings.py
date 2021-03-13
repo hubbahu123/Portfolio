5f2009b0f0fb84c316e4eaf0f3bd395771e13fa3
hubbahu123 <55627855+hubbahu123@users.noreply.github.com>
2021-03-13 12:14:19 -0500

    Debug has been set to false

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '$i&%6$*x$2a=rut63b#+$a71o4u7)r68f7(j49sab_#pc-#o(a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
